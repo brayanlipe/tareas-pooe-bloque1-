38ed13aff2faedc067331284fb226247fd96a4d6
brayan <you@example.com>
2024-09-21 10:23:45 -0600

    comparacion de edad

--- TareaBloque1/TareaBloque1/Module1.vb
+++ TareaBloque1/TareaBloque1/Module1.vb
@@ -3,6 +3,40 @@
     Sub Main()
         Console.WriteLine("estos son los programas de la tarea")
 
+        Dim edad As Integer
+        Dim mayor As Integer
+        Dim menor As Integer
+
+        mayor = 18
+        menor = 17
+
+        Console.WriteLine("ingrese la edad de la persona")
+        edad = Console.ReadLine()
+
+        If edad > menor Then
+            Console.WriteLine("la persona es mayor de edad")
+
+        Else
+            Console.WriteLine("la persona es menor de edad")
+
+
+        End If
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         Console.ReadKey()
 
