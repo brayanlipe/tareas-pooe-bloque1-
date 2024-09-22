1bbb46e05ba29ce5b7f39d5773b08e11a2b9403e
brayan <you@example.com>
2024-09-22 12:41:50 -0600

    numero positivo o negativo

---
+++ NumeroPositivoNegativo/NumeroPositivoNegativo/Module1.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Console.WriteLine("ingrese el numero para comparar si es positivo o negativo")
+        Dim numero As Integer
+        numero = Console.ReadLine()
+
+
+
+        If numero > 0 Then
+            Console.WriteLine("el numero es positivo")
+
+        ElseIf numero < 0 Then
+            Console.WriteLine("el numero es negativo")
+
+        Else
+            Console.WriteLine("el numero es 0")
+
+
+
+        End If
+
+
+
+
+
+        Console.ReadKey()
+
+    End Sub
+
+
+
+End Module
